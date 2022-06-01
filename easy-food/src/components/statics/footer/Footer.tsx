7e2c3b9d331987992f827b73b30801637dafd446
karolcoliveira <oliveiraakarol@hotmail.com>
2022-06-01 20:01:23 -0300

    @karolcoliveira [Task: Modificando Footer] {01/06/2022 20:01}

--- easy-food/src/components/statics/footer/Footer.tsx
+++ easy-food/src/components/statics/footer/Footer.tsx
@@ -1,27 +1,51 @@
+import React from 'react';
+import InstagramIcon from '@material-ui/icons/Instagram';
+import FacebookIcon from '@material-ui/icons/Facebook';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
 import './Footer.css';
 
 function Footer() {
     return (
         <>
             <footer id='footer_principal'>
-                <header>
-                    <img src="https://i.imgur.com/uLG82dK.png" alt="Logo" height='200px;' />
-                </header>
                 <section>
                     <article>
-                        <h2>Contato</h2>
+                        <h2>CONTATO</h2>
+                        <p>Horário de atendimento: 08 - 17 hrs</p>
                         <p>Endereço: Rua da Glória, 19304</p>
-                        <p>Telefone: 0000 - 0000</p>
-                        <p>Horário: 08 hrs - 17 hrs</p>
+                        <p>Telefone: (xx) 0000 - 0000</p>
+                        <p>Email: atendimento@EasyFood.com </p>
                     </article>
                     <article>
-                        <h2>Menu</h2>
-                        <ul>
-                            <li>Home</li>
-                            <li>Sobre</li>
-                            <li>Produtos</li>
-                            <li>Contato</li>
+                        <h2>LINKS ÚTEIS</h2>
+                        <ul id= 'menu'>
+                            <li>
+                                <a href="/home">Inicio</a>
+                            </li>
+                            <li>
+                                <a href="/about">Sobre</a>
+                            </li>
+                            <li>
+                                <a href="/products">Produtos</a>
+                            </li>
+                            <li>
+                                <a href="/contact">Contato</a>
+                            </li>
+                            <li>
+                                <a href="/login" >Login</a>
+                            </li>
                         </ul>
+                    </article>
+                    <article>
+                            <a href="https://www.facebook.com/" target="_blank" rel="noopener noreferrer">
+                                <FacebookIcon style={{ fontSize: 40, color: "#ECE7B4"}} />
+                            </a>
+                            <a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer">
+                                <InstagramIcon style={{ fontSize: 40, color: "#ECE7B4"}} />
+                            </a>
+                            <a href="https://www.linkedin.com/school/" target="_blank" rel="noopener noreferrer">
+                                <LinkedInIcon style={{ fontSize: 40, color: "#ECE7B4"}} />
+                            </a>
                     </article>
                 </section>
                 <footer>
@@ -31,8 +55,7 @@ function Footer() {
                 </footer>
             </footer>
         </>
-    )
-    
+    )   
 }
 
  export default Footer;
