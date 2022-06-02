ead4e49fb0ddedfceed1d128e37bc536f19922eb
CaptainSilvaBr <silva.asset@gmail.com>
2022-06-02 11:06:02 -0300

    @CaptainSilvaBr [Task:adicionando modelo de usuario e rotas ]{02\06\2022 11:05 }

--- easy-food/src/App.tsx
+++ easy-food/src/App.tsx
@@ -2,16 +2,39 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/statics/navbar/Navbar';
 import Footer from './components/statics/footer/Footer';
-import Home from './pages/home/Home'
+import Home from './pages/home/Home';
+import AboutUs from './pages/aboutus/AboutUs';
+import Products from './pages/products/Products';
+import Contact from './pages/contact/Contact';
+import Login from './pages/login/Login';
+import UserRegistration from './pages/userregistration/UserRegistration';
 import './App.css';
 
 function App() {
   return (
     <Router>
       <Navbar />
-      <Home />
+      <div style={{ minHeight: '100vh' }}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+
+          <Route path="/home" element={<Home />} />
+         
+          <Route path="/sobrenos" element={<AboutUs />} />
+
+          <Route path="/produtos" element={<Products />} />
+
+          <Route path="/contato" element={<Contact />} />
+
+          <Route path="/login" element={<Login />} />
+
+          <Route path="/cadastrousuario" element={<UserRegistration />} />
+
+        </Routes>
+      </div>
       <Footer />
     </Router>
+
   );
 }
 
---
+++ easy-food/src/models/User.ts
@@ -0,0 +1,11 @@
+interface User {
+    id?: number| null;
+    email?: string| null;
+    name?: string| null;
+    password?: string| null;
+    type?: string| null;
+    address?: string| null;
+    
+}
+
+export default User
