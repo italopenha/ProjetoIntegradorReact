956918a8cc81f9bfae1d4ec53c1e855bf7601d6f
joce-line <joceline.gutierrez@hotmail.com>
2022-06-07 13:56:22 -0300

    @joce-line [Task: Adição de search para ListProduct] {07/06/2022 13:55}

--- easy-food/src/services/Services.ts
+++ easy-food/src/services/Services.ts
@@ -13,3 +13,8 @@ export const userRegistration = async(url: any, _data: any, setData: any) => {
     const result = await api.post(url, _data)
     setData(result.data)
 }
+
+export const search = async(url: any, setData: any, header: any) => { 
+    const result = await api.get(url, header)
+    setData(result.data)
+}
