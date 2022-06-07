283055cf26fa58a57d39ea9ab86adcf5ac0e9395
karolcoliveira <oliveiraakarol@hotmail.com>
2022-06-07 15:04:55 -0300

    @karolcoliveira [Task: Adição do componente DeletePurchase]{07/06/2022 15:04}

---
+++ easy-food/src/components/purchases/deletePurchase/DeletePurchase.tsx
@@ -0,0 +1,89 @@
+import React, { useEffect, useState } from 'react'
+import { Typography, Button, Card, CardActions, CardContent } from "@material-ui/core"
+import './DeletePurchase.css';
+import { useNavigate, useParams } from 'react-router-dom';
+import { searchId, deleteId } from '../../../services/Services';
+import { Box } from '@mui/material';
+import { useSelector } from 'react-redux';
+import { TokenState } from '../../../store/tokens/tokensReducer';
+import NewPurchaseDTO from '../../../models/NewPurchaseDTO';
+
+function DeletePurchase() {
+
+    let navigate = useNavigate();
+    const { id } = useParams<{ id: string }>();
+    const [purchase, setPurchase] = useState<NewPurchaseDTO>()
+    const token = useSelector<TokenState, TokenState["tokens"]>(
+        (state) => state.tokens
+      );
+
+    useEffect(() => {
+        if (token == "") {
+            alert("Você precisa estar logado")
+            navigate("/login")
+
+        }
+    }, [token])
+
+    useEffect(() => {
+        if (id !== undefined) {
+            findById(id)
+        }
+    }, [id])
+
+    async function findById(id: string) {
+        searchId(`/api/Purchases/id/${id}`, setPurchase, {
+            headers: {
+                'Authorization': token
+            }
+        })
+    }
+
+    function sim() {
+        navigate('/compras')
+        deleteId(`/api/Purchases/delete/${id}`, {
+            headers: {
+                'Authorization': token
+            }
+        });
+        alert('Compra deletada com sucesso');
+    }
+
+    function nao() {
+        navigate('/compras')
+    }
+    return (
+        <>
+            <Box m={2}>
+                <Card variant="outlined" >
+                    <CardContent>
+                        <Box justifyContent="center">
+                            <Typography color="textSecondary" gutterBottom>
+                                Deseja deletar a compra:
+                            </Typography>
+                            <Typography color="textSecondary" >
+                                {purchase?.nameItems}
+                            </Typography>
+                        </Box>
+
+                    </CardContent>
+                    <CardActions>
+                        <Box display="flex" justifyContent="start" ml={1.0} mb={2} >
+                            <Box mx={2}>
+                                <Button onClick={sim} variant="contained" className="marginLeft" size='large' color="primary">
+                                    Sim
+                                </Button>
+                            </Box>
+                            <Box>
+                                <Button onClick={nao} variant="contained" size='large' color="secondary">
+                                    Não
+                                </Button>
+                            </Box>
+                        </Box>
+                    </CardActions>
+                </Card>
+            </Box>
+        </>
+    );
+}
+export default DeletePurchase;
