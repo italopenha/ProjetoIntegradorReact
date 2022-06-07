684e2c64a211eb5e5d4b3272c417f4a750054747
karolcoliveira <oliveiraakarol@hotmail.com>
2022-06-07 14:13:31 -0300

    @karolcoliveira [Task: Atualização em Services] {07/06/2022 14:13

--- easy-food/src/services/Services.ts
+++ easy-food/src/services/Services.ts
@@ -18,3 +18,22 @@ export const search = async(url: any, setData: any, header: any) => {
     const result = await api.get(url, header)
     setData(result.data)
 }
+
+export const searchId = async(url: any, setDada: any, header: any) => { 
+    const result = await api.get(url, header)
+    setDada(result.data)
+}
+
+export const post = async(url: any, _data: any, setData: any, header: any) => { 
+    const result = await api.post(url, _data, header)
+    setData(result.data)
+}
+
+export const put = async(url: any, _data: any, setData: any, header: any) => { 
+    const result = await api.put(url, _data, header)
+    setData(result.data)
+}
+
+export const deleteId = async(url: any, header: any) => { 
+    await api.delete(url, header)
+}
